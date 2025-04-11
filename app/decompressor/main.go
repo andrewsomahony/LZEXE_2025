e4208247e39c7c2764a92b20216462b6792d549f
Andrew O'Mahony <andrewsomahony@gmail.com>
2025-04-10 20:45:28 -0500

    Fourth try

--- app/decompressor/main.go
+++ app/decompressor/main.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
-import "github.com/andrewsomahony/LZEXE_2025/decompressor/routine"
+import (
+	"fmt"
+
+	"github.com/andrewsomahony/LZEXE_2025/decompressor/routine"
+)
 
 func main() {
 	fmt.Println("This is the decompressor app")
